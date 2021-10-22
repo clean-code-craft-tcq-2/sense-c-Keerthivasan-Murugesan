2bffae9a17b532b38a6eeb43fea14047b2bf2d88
Keerthivasan-Murugesan <91077090+Keerthivasan-Murugesan@users.noreply.github.com>
2021-10-23 00:06:10 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -7,7 +7,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     total = min = max = 0;
     for (i=0; i++; i<setlength)
     {
-        total = total + numberset[i]
+        total = total + numberset[i];
         if(numberset[i] > max) max = numberset[i];
         if(numberset[i] < min) min = numberset[i];
     }
