ec6eed340112fe0fd3b3e24f88994b7bb1ff117a
Keerthivasan-Murugesan <91077090+Keerthivasan-Murugesan@users.noreply.github.com>
2021-10-23 18:29:25 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -23,9 +23,9 @@ TEST_CASE("average is NaN for empty array") {
     
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
-    REQUIRE(isnan(computedStats.average));
-    REQUIRE(isnan(computedStats.max));
-    REQUIRE(isnan(computedStats.min));
+    REQUIRE(std::isnan(computedStats.average));
+    REQUIRE(std::isnan(computedStats.max));
+    REQUIRE(std::isnan(computedStats.min));
 }
 /*
 TEST_CASE("raises alerts when max is greater than threshold") {
