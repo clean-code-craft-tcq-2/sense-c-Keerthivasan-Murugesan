c4ad3b9059464b87b4402b7966c6fc11abb965fb
Keerthivasan-Murugesan <91077090+Keerthivasan-Murugesan@users.noreply.github.com>
2021-10-23 17:10:54 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -10,7 +10,8 @@ TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
     struct Stats computedStats = compute_statistics(numberset, setlength);
-    Console.WriteLine(computedStats.average, computedStats.max, computedStats.min);
+    printf("hello");
+    printf("%f, %f, %f", computedStats.average, computedStats.max, computedStats.min);
     float epsilon = 0.001;
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
