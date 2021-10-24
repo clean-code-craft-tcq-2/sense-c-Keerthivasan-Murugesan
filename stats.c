4d09d7558649ce8de10a80752ed8b06493c6b8dd
Keerthivasan-Murugesan <keerthim226@gmail.com>
2021-10-24 13:02:29 +0530

    Updated alerter functions

--- stats.c
+++ stats.c
@@ -11,7 +11,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.min = 0;
     s.max = 0;
       
-    if((setlength == 0) || (numberset == 0) || (numberset == NULL))
+    if((setlength == 0) || (*numberset == NULL))
     {
         s.average = NAN;
         s.min = NAN;
@@ -20,30 +20,28 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     else
     {      
         s.average = 0;
-        s.min = numberset[i];
-        s.max = numberset[i];
+        s.min = s.max = numberset[i];
         
         for (i=0; i<setlength; i++)
         {
-            s.average = s.average + numberset[i];
+            s.average += numberset[i];
             if(numberset[i] < s.min) s.min = numberset[i];
             if(numberset[i] > s.max) s.max = numberset[i];
         }
-        s.average = s.average / setlength;
+        s.average /= setlength;
     }
     
-    return s;
-    
+    return s;   
 }
 
-void emailAlerter(void)
+void emailAlerter()
 {
-    emailAlertCallCount = 1;
+    emailAlertCallCount++;
 }
 
-void ledAlerter(void)
+void ledAlerter()
 {
-    ledAlertCallCount = 1;
+    ledAlertCallCount++;
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
